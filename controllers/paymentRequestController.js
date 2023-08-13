bd21e813bc22696582450edf60199e6d2a622a15
Abhishek <ac12644@gmail.com>
2023-08-13 19:03:26 +0200

    Added comments

--- controllers/paymentRequestController.js
+++ controllers/paymentRequestController.js
@@ -1,35 +1,45 @@
 const qrcode = require("qrcode");
 const fs = require("fs");
 const axios = require("axios");
-
 const { LocalStorage } = require("node-localstorage");
 
-// Create a local storage instance with a specific directory path
+// Create a local storage instance with a specific directory path to save QR codes.
 const localStorage = new LocalStorage("./qrCodes");
 
-// Function to generate a payment request QR code
+/**
+ * Generate a Bitcoin payment request QR code.
+ * The generated QR code encodes a Bitcoin payment request which can be scanned by Bitcoin wallets for ease of payment.
+ *
+ * @param {Object} req - The express request object.
+ * @param {Object} req.query - The request query parameters.
+ * @param {string} req.query.address - The Bitcoin address where funds should be sent.
+ * @param {string} req.query.amount - The amount in Bitcoin to request.
+ * @param {string} req.query.message - An optional message for the payment request.
+ *
+ * @returns {Object} A JSON response containing the generated QR code data, file path, and URL.
+ */
 exports.generatePaymentRequestQR = async (req, res) => {
   const { address, amount, message } = req.query;
 
   try {
-    // Generate the payment request string
+    // Construct the payment request string in the BIP21 format.
     const paymentRequest = `bitcoin:${address}?amount=${amount}&message=${message}`;
 
-    // Generate the QR code image as a data URL
+    // Use the 'qrcode' library to generate the QR code image as a data URL.
     const qrCode = await qrcode.toDataURL(paymentRequest);
 
-    // Store the QR code image in the local storage
+    // Store the generated QR code image in local storage for later retrieval.
     const qrCodeId = Date.now().toString();
     const qrCodeKey = `qrCode_${qrCodeId}`;
     localStorage.setItem(qrCodeKey, qrCode);
 
-    // Save the QR code image as a PNG file with a unique code name
+    // Save the generated QR code image to a PNG file on disk.
     const qrCodeImageFileName = `payment-request-qr-${qrCodeId}.png`;
     const qrCodeImageFilePath = `./qrCodes/${qrCodeImageFileName}`;
     const qrCodeImage = qrCode.replace(/^data:image\/png;base64,/, "");
     fs.writeFileSync(qrCodeImageFilePath, qrCodeImage, "base64");
 
-    // Generate a shareable QR code using an online service
+    // Use an external QR code generation service to get another version of the QR code.
     const qrCodeAPIUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
       paymentRequest
     )}`;
@@ -39,9 +49,9 @@ exports.generatePaymentRequestQR = async (req, res) => {
     const qrCodeImageBase64 = Buffer.from(response.data, "binary").toString(
       "base64"
     );
-    const qrCodeImageUrl = qrCodeAPIUrl; // Use the QR code URL received from the service
+    const qrCodeImageUrl = qrCodeAPIUrl; // Use the QR code URL received from the service.
 
-    // Return the QR code image data, file path, and URL in JSON format
+    // Return the QR code details in the response.
     res.json({
       success: true,
       qrCodeId,
--- controllers/sendBtcController.js
+++ controllers/sendBtcController.js
@@ -2,21 +2,33 @@ require("dotenv").config();
 const bitcore = require("bitcore-lib");
 const axios = require("axios");
 
+/**
+ * Send Bitcoin to a specified address.
+ *
+ * @param {Object} req - The express request object.
+ * @param {Object} req.body - The request body parameters.
+ * @param {string} req.body.amount - The amount in Bitcoin to send.
+ * @param {string} req.body.to - The Bitcoin address to send funds to.
+ *
+ * @param {Object} res - The express response object.
+ * @returns {Object} A JSON response containing the transaction ID if successful.
+ */
 exports.sendBitcoin = async (req, res) => {
   try {
+    // Initialize for the Bitcoin test network
     const testnet = bitcore.Networks.testnet;
+
+    // Load the Bitcoin private key from environment variables
     const privateKey = new bitcore.PrivateKey.fromWIF(
       process.env.BITCOIN_PRIVATE_KEY,
       testnet
     );
     const address = privateKey.toAddress(testnet);
 
+    // Fetch the unspent transaction outputs (UTXOs) for the given address
     const { data: utxos } = await axios.get(
       `https://blockstream.info/testnet/api/address/${address}/utxo`
     );
-
-    let totalAmountAvailable = 0;
-    const transaction = new bitcore.Transaction();
 
     if (utxos.length === 0) {
       return res
@@ -24,10 +36,15 @@ exports.sendBitcoin = async (req, res) => {
         .send({ error: "No UTXOs found for this address." });
     }
 
+    let totalAmountAvailable = 0;
+    const transaction = new bitcore.Transaction();
+
+    // Get the public key from the private key to generate the scriptPubKey
     const publicKey = bitcore.PublicKey.fromPrivateKey(privateKey);
     const scriptPubKey =
       bitcore.Script.buildPublicKeyHashOut(publicKey).toString();
 
+    // Loop through each UTXO to include in our transaction
     utxos.forEach((utxo) => {
       transaction.from({
         txId: utxo.txid,
@@ -42,19 +59,21 @@ exports.sendBitcoin = async (req, res) => {
     const fee = 50000;
     const remaining = totalAmountAvailable - satoshisToSend - fee;
 
+    // Check for sufficient balance to send the transaction
     if (remaining < 0) {
       return res.status(400).send({ error: "Insufficient balance." });
     }
 
+    // Build the transaction
     transaction.to(req.body.to, satoshisToSend);
     transaction.change(address);
     transaction.fee(fee);
     transaction.sign(privateKey);
 
+    // Serialize the constructed transaction
     const serializedTransaction = transaction.serialize();
 
-    // Blockstream doesn't provide an API for broadcasting transactions,
-    // so you can use blockcypher's API for this
+    // Use the blockcypher API to broadcast the transaction
     const { data: result } = await axios.post(
       "https://api.blockcypher.com/v1/btc/test3/txs/push",
       {
@@ -62,6 +81,7 @@ exports.sendBitcoin = async (req, res) => {
       }
     );
 
+    // Return the transaction ID
     res.send({ txId: result.tx.hash });
   } catch (error) {
     console.error(error);

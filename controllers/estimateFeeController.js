5d73ad5e59a0f0306533a1e3d8b21f30a536da27
Abhishek <ac12644@gmail.com>
2024-01-13 01:24:20 +0530

    updated estimated fee response

--- controllers/estimateFeeController.js
+++ controllers/estimateFeeController.js
@@ -10,7 +10,18 @@ exports.estimateFee = async (req, res) => {
     const response = await axios.get(
       `https://blockstream.info/api/fee-estimates`
     );
-    res.json(response.data);
+
+    // Calculate the average fee for the next 6 blocks
+    let totalFee = 0;
+    for (let i = 1; i <= 6; i++) {
+      totalFee += response.data[i];
+    }
+    const averageFeeSatoshis = totalFee / 6;
+
+    // Convert the average fee to BTC (1 BTC = 100,000,000 Satoshis)
+    const averageFeeBTC = averageFeeSatoshis / 100000000;
+
+    res.json({ averageFeeBTC, averageFeeSatoshis });
   } catch (error) {
     console.error("Error fetching fee estimates:", error);
     res.status(500).json({ error: "Failed to fetch fee estimates" });
